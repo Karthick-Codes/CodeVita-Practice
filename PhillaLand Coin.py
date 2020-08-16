ace925bdd60bb089a757653b29dd0f3757498562
Karthick-Codes <53971182+Karthick-Codes@users.noreply.github.com>
2020-08-16 19:22:48 +0530

    Create PhillaLand Coin.py

---
+++ PhillaLand Coin.py
@@ -0,0 +1,13 @@
+PhillaLand Coin 
+
+inp=int(input())
+for i in range(inp):
+    print(int(input()).bit_length())
+
+Program Logic
+	find minimum coin for a price.
+Program detailed Explaination:
+	For any price in between (1-10) 10. manisha need to give money and she need carry minimunm coins.
+ 8421 with is we can give any number in between 1-10
+ultimately we need binary length of a number.
+	**bit_length function can give binary length of a number.**
